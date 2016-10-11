6831c4cf4519bd687578d534a0c1193d71261ec4
dotnetjunkie <steven@cuttingedge.it>
2016-10-11 21:37:27 +0200

    Pure DI project added.

---
+++ src/SampleApplication.PureDI/CompositionRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleApplication.PureDI.Controllers;
+
+namespace SampleApplication.PureDI
+{
+    public class CompositionRoot
+    {
+        private readonly IApplicationBuilder app;
+        private readonly ILoggerFactory loggerFactory;
+
+        public CompositionRoot(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            this.app = app;
+            this.loggerFactory = loggerFactory;
+        }
+
+        public Controller CreateController(Type controllerType)
+        {
+            var userService = new AspNetUserService();
+
+            if (controllerType == typeof(HomeController))
+            {
+                return new HomeController(userService);
+            }
+
+            throw new InvalidOperationException("Unknown type " + controllerType.FullName);
+        }
+
+        public CustomMiddleware CreateCustomMiddleware() => new CustomMiddleware(loggerFactory, new AspNetUserService());
+    }
+}
---
+++ src/SampleApplication.PureDI/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MissingDIExtensions;
+
+namespace SampleApplication.PureDI
+{
+    public class Startup
+    {
+        private CompositionRoot compositionRoot;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            services.AddCustomControllerActivation(CreateController);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            compositionRoot = new CompositionRoot(app, loggerFactory);
+
+            // Add custom middleware
+            app.Use(async (context, next) =>
+            {
+                await compositionRoot.CreateCustomMiddleware().Invoke(context, next);
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        private Controller CreateController(Type type) => compositionRoot.CreateController(type);
+    }
+
+    public interface IUserService { }
+
+    public class AspNetUserService : IUserService { }
+}
