7efd3c3f1aa6330d303d0a3e2d28fdc43687b9f4
dotnetjunkie <steven@cuttingedge.it>
2016-12-13 18:19:28 +0100

    Changed HomeController
    Added dependency to HomeController constructor in Simple Injector
    example to show the use of the Func<IViewBufferScope> registration.

--- src/SampleApplication.SimpleInjector/Controllers/HomeController.cs
+++ src/SampleApplication.SimpleInjector/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 
 namespace SampleApplication.SimpleInjector.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Func<IViewBufferScope> scopeProvider;
+
+        public HomeController(Func<IViewBufferScope> scopeProvider)
+        {
+            this.scopeProvider = scopeProvider;
+        }
+
         public IActionResult Index()
         {
+            var scope = this.scopeProvider();
             return View();
         }
 
